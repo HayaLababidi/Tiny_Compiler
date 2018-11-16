e7f9b601f4efd8e25efaa3c5cec043b713cb8db6
maioy97 <mai.algendyfcis@gmail.com>
2018-11-16 14:56:33 +0200

    math part done

--- TINY_Compiler_Scanner/JASON_Compiler/SyntaxAnalyser.cs
+++ TINY_Compiler_Scanner/JASON_Compiler/SyntaxAnalyser.cs
@@ -19,13 +19,16 @@ namespace JASON_Compiler
         {
             Node root= new Node();
             root.token = new Token();
-            
-            //write your parser code
 
+            //write your parser code
+            root.children.Add(Match(Token_Class.Program, Tokens));
+            root.children.Add(Header(Tokens));
+            root.children.Add(DecSec(Tokens));
+            root.children.Add(Block(Tokens));
             return root;
         }
 
-        public static Node match(Token_Class expected_tok,List<Token> Tokens)
+        public static Node Match(Token_Class expected_tok,List<Token> Tokens)
         {
             if (expected_tok == Tokens[index].token_type)
             {
@@ -40,6 +43,132 @@ namespace JASON_Compiler
                 Node terminal = new Node();
                 return null;
             }
+        }
+
+        public static Node Exp(List<Token> Tokens)
+        {
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Exp";
+            returned_node.children.Add(Term(Tokens));
+            returned_node.children.Add(Exp_(Tokens));
+            return returned_node;
+        }
+        public static Node Exp_(List<Token> Tokens)
+        {// Add_op Term Exp’ | ε
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Exp";
+            if (Tokens[index].token_type == Token_Class.MinusOp|| Tokens[index].token_type == Token_Class.PlusOp)
+            {// Add_op Term Exp’
+                returned_node.children.Add(Add_op(Tokens));
+                returned_node.children.Add(Term(Tokens));
+                returned_node.children.Add(Exp_(Tokens));
+            }
+            //| ε
+            return returned_node;
+        }
+        public static Node Add_op(List<Token> Tokens)
+        {
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Addop";
+            if (Tokens[index].token_type == Token_Class.MinusOp )
+            {
+                returned_node.children.Add(Match(Token_Class.MinusOp, Tokens));
+            }
+            else if (Tokens[index].token_type == Token_Class.PlusOp)
+            {
+                returned_node.children.Add(Match(Token_Class.MinusOp, Tokens));
+            }
+            else
+            {
+                returned_node.token.lex = "ERROR";
+                return returned_node;
+            }
+            return returned_node;
+        }
+        public static Node Mul_op(List<Token> Tokens)
+        {
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Mulop";
+            if (Tokens[index].token_type == Token_Class.MultiplyOp)
+            {
+                returned_node.children.Add(Match(Token_Class.MultiplyOp, Tokens));
+            }
+            else if (Tokens[index].token_type == Token_Class.DivideOp)
+            {
+                returned_node.children.Add(Match(Token_Class.DivideOp, Tokens));
+            }
+            else
+            {
+                returned_node.token.lex = "ERROR";
+                return returned_node;
+            }
+            return returned_node;
+        }
+        public static Node Term(List<Token> Tokens)
+        {
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Term";
+            returned_node.children.Add(Factor(Tokens));
+            returned_node.children.Add(Term_(Tokens));
+            return returned_node;
+        }
+        public static Node Term_(List<Token> Tokens)
+        {// Term’ → Mul-op Factor Term’ | ε 
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Exp";
+            if (Tokens[index].token_type == Token_Class.DivideOp|| Tokens[index].token_type == Token_Class.MultiplyOp)
+            {// Mul-op Factor Term’
+                returned_node.children.Add(Mul_op(Tokens));
+                returned_node.children.Add(Factor(Tokens));
+                returned_node.children.Add(Term_(Tokens));
+            }
+            //| ε
+            return returned_node;
+        }
+        public static Node Factor(List<Token> Tokens)
+        {// Factor → (Exp) | number
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Factor";
+            if (Tokens[index].token_type == Token_Class.Number)
+            {// Mul-op Factor Term’
+                returned_node.children.Add(Match(Token_Class.Number,Tokens));
+            }
+            else
+            {
+                returned_node.children.Add(Match(Token_Class.LParanthesis, Tokens));
+                returned_node.children.Add(Exp(Tokens));
+                returned_node.children.Add(Match(Token_Class.RParanthesis, Tokens));
+            }
+            //| ε
+            return returned_node;
+        }
+        public static Node Assignment_stmnt(List<Token> Tokens)
+        {//Assignment statement →  Identifier Assignment_operator exp 
+
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Assignment statement";
+            returned_node.children.Add(Match(Token_Class.Identifier,Tokens));
+            returned_node.children.Add(Assignment_op(Tokens));
+            returned_node.children.Add(Exp(Tokens));
+
+            return returned_node;
+        }
+        public static Node Assignment_op(List<Token> Tokens)
+        {
+            Node returned_node = new Node();
+            returned_node.token = new Token();
+            returned_node.token.lex = "Assignment operator";
+            index++;// cause match will only match to one of them 
+            returned_node.children.Add(Match(Token_Class.AssignmentOp, Tokens));
+            return returned_node;
         }
         //use this function to print the parse tree in TreeView Toolbox
         public static TreeNode PrintParseTree(Node root)
